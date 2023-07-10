9585365da1f780509bad4acac429f0ca1b468333
Joseph Wondwosen <arthurtefera@gmail.com>
2023-07-10 22:50:03 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-900 flex flex-col min-h-screen justify-center items-center text-slate-100">
+      Hello from vite
+    </div>
+  );
+}
+
+export default App;
